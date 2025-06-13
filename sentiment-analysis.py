eee7715a110681711fc5494357f72bba758c44ee
thynash <24mc3033@rgipt.ac.in>
2025-06-13 05:43:00 +0000

    Sentiment-analysis done

---
+++ sentiment-analysis.py
@@ -0,0 +1,14 @@
+from textblob import TextBlob
+
+def get_sentiment(text):
+    blob = TextBlob(text)
+    polarity = blob.sentiment.polarity
+
+    if polarity > 0.1:
+        sentiment = "positive"
+    elif polarity < -0.1:
+        sentiment = "negative"
+    else:
+        sentiment = "neutral"
+    return sentiment, round(polarity, 3)
+
